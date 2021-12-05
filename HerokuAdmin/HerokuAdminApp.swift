d09b51a0d3e1e6e5fd1ae2796a9cc29033800f2c
Malvere <gatling1404@gmail.com>
2021-12-06 00:23:48 +0300

    Initial Commit

---
+++ HerokuAdmin/HerokuAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  HerokuAdminApp.swift
+//  HerokuAdmin
+//
+//  Created by Kostya Kuznetsov on 06.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct HerokuAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
