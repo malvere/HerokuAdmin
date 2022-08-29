548a4ef993d390aed449161f6c983e292f400bb6
Malvere <gatling1404@gmail.com>
2022-08-29 21:58:51 +0300

    1

--- HerokuAdmin/HerokuAdminApp.swift
+++ HerokuAdmin/HerokuAdminApp.swift
@@ -11,9 +11,7 @@ import SwiftUI
 struct HerokuAdminApp: App {
     var body: some Scene {
         WindowGroup {
-            NavigationView {
-                ContentView()
-            }
+            ContentView()
         }
     }
 }
