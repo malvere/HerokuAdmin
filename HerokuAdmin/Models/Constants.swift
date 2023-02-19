f44ae497c0a71dc61f861f9a7082c1caddf61706
Malvere <gatling1404@gmail.com>
2023-02-20 01:16:30 +0300

    2

---
+++ HerokuAdmin/Models/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  HerokuAdmin
+//
+//  Created by Kostya Kuznetsov on 22.12.2021.
+//
+
+// Keeping constants in one place
+import Foundation
+
+struct Constants {
+    static let keychainKey: String = "com.malverecorp.HerokuAdmin.keychain"
+}
+
